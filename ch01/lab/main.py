93aeaed55e5ef3d1a297bf2c1839fcf75418a88a
Alex Szczepanski <aszczep1@binghamton.edu>
2023-01-30 23:58:53 -0500

    lab 1

--- ch01/lab/main.py
+++ ch01/lab/main.py
@@ -1,1 +1,17 @@
+import random
+# Part A
+weeks = 16
+classes = 5
+tuition = 6000
+cost_per_week = ((tuition / classes) / weeks)
+print("Cost per week:", cost_per_week)
 
+# Part B
+var1= "hi"
+var2 = "bye"
+var3 = "no"
+var4 = "yeah" 
+var5 = '4?'
+mylist = [var1, var2, var3, var4, var5]
+answer = random.choice(mylist)
+print(answer)
