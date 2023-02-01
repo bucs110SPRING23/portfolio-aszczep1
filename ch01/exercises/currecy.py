a396bb2907b087a328b1a6e9b136b4dcb9608c93
Alex Szczepanski <aszczep1@binghamton.edu>
2023-02-01 17:57:55 -0500

    yeah

---
+++ ch01/exercises/currecy.py
@@ -0,0 +1,5 @@
+rate = float(input("What is the Euro to Dollar exchange rate?"))
+amount = float(input("How much would you like to exchange?"))
+total = rate * amount
+result = total - 3
+print(result) 
