ad966d3d5a1f1c5d4ab0f7c35194d9220cec166e
Alex Szczepanski <aszczep1@binghamton.edu>
2023-03-21 00:03:05 -0400

    yeah

--- ch05/lab/main.py
+++ ch05/lab/main.py
@@ -27,20 +27,29 @@ def threenp1range(upper_limit):
 def graph_coordinates(threenplus1_inters_dict): 
     screen = pygame.display.set_mode()
     screen_size = screen.get_size()
-    pygame.screen_fill("blue")
-    
-    inters_dict = threenplus1_inters_dict.items()
-    pygame.draw.lines(screen, "red", False, inters_dict)
-
-
+    screen.fill("blue")
     new_display = pygame.transform.flip(screen, False, True) 
     width, height = new_display.get_size()
     new_display = pygame.transform.scale(new_display, (width * 5, height * 5))
     screen.blit(new_display, (0,0))
 
-    font = pygame.font.Font(font_name, size) 
-    msg = font.render(msg, antialias, color) 
+    
+    inters_dict = threenplus1_inters_dict.items()
+    print(inters_dict)
+    max_so_far = 0 
+    cor_1 = (0, 0)
+    for v, k in inters_dict:
+        cor_2 = (v, k)
+        pygame.draw.lines(screen, "red", cor_1, cor_2)
+        cor_1 = (v, k)
 
+    
+    font = pygame.font.Font(None, 48)
+    words = [
+        f"The max so far is", {max_so_far}
+         ]
+    msg = font.render(words, False, "red") 
+    pos = (10, 10)
     screen.blit(msg, pos) 
 
 
@@ -53,5 +62,6 @@ def main():
     rangdict = threenp1range(upper_limit)
     print(rangdict)
     pygame.init()
+    graph_coordinates(rangdict)
     
 main()
